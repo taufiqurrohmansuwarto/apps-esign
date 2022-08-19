10a242e3cb5f0a47cb5c9cc9454e338ee2a153e9
Iput <taufiqurrohman.suwarto@gmail.com>
2022-08-20 06:51:20 +0700

    add ip info

--- pages/api/testing.js
+++ pages/api/testing.js
@@ -2,6 +2,7 @@ import { createRouter } from "next-connect";
 import {
   expressCompression,
   expressFingerPrint,
+  expressIp,
 } from "../../middleware/express-middleware";
 
 const router = createRouter();
@@ -9,17 +10,15 @@ const router = createRouter();
 router
   .use(expressCompression)
   .use(expressFingerPrint)
+  .use(expressIp)
   .get((req, res) => {
     var ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
-    res
-      .status(200)
-      .json({
-        fingerprint: req?.fingerprint,
-        ip,
-        ipInf: req?.ipInfo,
-        helo: "world",
-      });
+    res.status(200).json({
+      fingerprint: req?.fingerprint,
+      ip,
+      ipInfo: req?.ipInfo,
+    });
   });
 
 export default router.handler();
