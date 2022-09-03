2e9b1c307914c33daf4be1deb63c276a21a92864
Iput <taufiqurrohman.suwarto@gmail.com>
2022-09-03 07:47:34 +0700

    add description to head

--- pages/signin.js
+++ pages/signin.js
@@ -1,38 +1,46 @@
-import { Button, Col, Divider, Layout, Row, Typography } from "antd";
+import { Button, Col, Divider, Row, Typography } from "antd";
 import { getProviders, signIn } from "next-auth/react";
+import Head from "next/head";
 import Link from "next/link";
 
 export default function SignIn({ providers }) {
   return (
-    <Row
-      style={{
-        height: "100vh",
-        display: "flex",
-      }}
-      align="middle"
-      justify="center"
-    >
-      <Col>
-        <Typography.Title>E-Sign</Typography.Title>
-        {Object?.values(providers).map((provider) => (
-          <div key={provider.name}>
-            <Button type="primary" onClick={() => signIn(provider.id)}>
-              Masuk dengan akun {provider.name}
-            </Button>
-          </div>
-        ))}
-        <Divider />
-        <Typography.Text>
-          <Link href="/check-document">
-            <a>Ingin cek dokumen?</a>
-          </Link>
-        </Typography.Text>
-      </Col>
-    </Row>
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Aplikasi Tanda Tangan Elektronik BKD Jatim"
+        />
+      </Head>
+      <Row
+        style={{
+          height: "100vh",
+          display: "flex",
+        }}
+        align="middle"
+        justify="center"
+      >
+        <Col>
+          <Typography.Title>E-Sign</Typography.Title>
+          {Object?.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <Button type="primary" onClick={() => signIn(provider.id)}>
+                Masuk dengan akun {provider.name}
+              </Button>
+            </div>
+          ))}
+          <Divider />
+          <Typography.Text>
+            <Link href="/check-document">
+              <a>Ingin cek dokumen?</a>
+            </Link>
+          </Typography.Text>
+        </Col>
+      </Row>
+    </>
   );
 }
 
-// This is the recommended way for Next.js 9.3 or newer
 export async function getServerSideProps() {
   const providers = await getProviders();
   return {
